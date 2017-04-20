e98c0591199f9175fac99ffa23264d2393e9463e
ashleeryland <rylandashlee@gmail.com>
2017-04-20 20:59:08 +1000

    Changed colours

--- app/src/main/java/com/example/ashlee/unitconverter/ChangeSettingsActivity.java
+++ app/src/main/java/com/example/ashlee/unitconverter/ChangeSettingsActivity.java
@@ -4,24 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 public class ChangeSettingsActivity extends AppCompatActivity {
-
-    private TextView secondResult;
-    private EditText secondValueToConvert;
-    private Spinner secondSpinnerSelect1;
-    private Spinner secondSpinnerSelect2;
-    public Button secondDubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_settings);
-
     }
 
     public void metricScreen(View view) {
